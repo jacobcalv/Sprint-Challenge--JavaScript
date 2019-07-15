3c1f0883feeaed9179e399f13f9af35b409d7a45
jacobcalv <jacobcalvino@outlook.com>
2019-07-15 18:58:05 -0500

    finish edits

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -68,7 +68,8 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-graduates.forEach(graduate => (universities.push(graduate['first_name'])));
+graduates.forEach(graduate => (universities.push(graduate['university'])))
+universities.sort();
 
 console.log(universities)
 
